21fc5abe4bf5d7d858e6cf757c3f46b7b3b50190
karthikiitm87 <karthikiitm87@gmail.com>
2024-03-10 11:45:33 +0000

    codes for training DGFRCNN for GWHD

--- train_GWHD_dgfrcnn.py
+++ train_GWHD_dgfrcnn.py
@@ -54,17 +54,11 @@ class WheatDataset(Dataset):
         self.image_set = image_set
         self.image_path = root_dir+annotations["image_name"]
         self.boxes = [self.decodeString(item) for item in annotations["BoxesString"]]
-        self.domains_str = annotations['domain']
-        
-        if(image_set == 'train'):
-          self._domains = ('0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15', '16', '17')
-        elif(image_set == 'val'):
-          self._domains = ('18', '19', '20', '21', '22', '23', '24', '25')
-        else:
-          self._domains = ('26', '27', '28', '29', '30', '31', '32', '33', '34', '35', '36', '37', '38', '39', '40', '41', '42', '43', '44', '45', '46')
-        
-        self.num_domains = len(self._domains)
-        self._domain_to_ind = dict(zip(self._domains, range(len(self._domains))))
+        #self.domains_str = annotations['domain']
+        unique_values = annotations['domain'].unique()
+        unique_indices = {value: index for index, value in enumerate(unique_values)}
+        self.domain_index = annotations['domain_index'] = annotations['domain'].map(unique_indices)
+       
         self.transform = transform
 
     def __len__(self):
@@ -77,11 +71,9 @@ class WheatDataset(Dataset):
         img = cv2.imread(imgp)
         image = cv2.cvtColor(img, cv2.COLOR_BGR2RGB) # Opencv open images in BGR mode by default
         
-        try:
-          domain = torch.tensor(self._domain_to_ind[str(self.domains_str[idx])])
-        except:
-          domain = torch.tensor(-1)
-          
+        domain = torch.tensor(self.domain_index[idx])
+       
+                   
         if self.transform:
           bboxes[:, 0][bboxes[:, 0] == bboxes[:, 2]] = bboxes[:, 0][bboxes[:, 0] == bboxes[:, 2]] - 1
           bboxes[:, 1][bboxes[:, 1] == bboxes[:, 3]] = bboxes[:, 1][bboxes[:, 1] == bboxes[:, 3]] - 1
@@ -156,7 +148,7 @@ def collate_fn(batch):
         domain_labels.append(d)
     images = torch.stack(images, dim=0)
 
-    return images, targets, domain_labels, orig_img
+    return images, targets, torch.tensor(domain_labels), orig_img
 
 
 class GRLayer(Function):
@@ -285,11 +277,13 @@ class DGFRCNN(LightningModule):
         self.n_classes = n_classes
         self.batchsize = batchsize
         self.exp = exp
+        self.reg_weights = reg_weights
+        self.num_domains = 18
 	
-        self.ImageDA = _ImageDA(256, 18)
-        self.InsDA = _InstanceDA(18)       
-        self.InsCls = nn.ModuleList([_InsCls(self.n_classes) for i in range(18)])
-        self.InsClsPrime = nn.ModuleList([_InsClsPrime(self.n_classes) for i in range(18)])
+        self.ImageDA = _ImageDA(256, self.num_domains)
+        self.InsDA = _InstanceDA(self.num_domains)       
+        self.InsCls = nn.ModuleList([_InsCls(self.n_classes) for i in range(self.num_domains)])
+        self.InsClsPrime = nn.ModuleList([_InsClsPrime(self.n_classes) for i in range(self.num_domains)])
 
         self.metric = MeanAveragePrecision(iou_type="bbox", class_metrics=True, iou_thresholds = [0.5])
         self.base_lr = 1e-4 #Original base lr is 1e-4
@@ -301,7 +295,6 @@ class DGFRCNN(LightningModule):
         self.detector.roi_heads.box_head.register_forward_hook(self.store_ins_features)
         
         self.mode = 0
-        self.sub_mode = 0
     
     def store_ins_features(self, module, input1, output):
       self.box_features = output
@@ -326,8 +319,8 @@ class DGFRCNN(LightningModule):
                                     {'params': self.InsClsPrime.parameters(), 'lr': self.base_lr, 'weight_decay': self.weight_decay}
                                       ],) 
       
-      lr_scheduler = {'scheduler': torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer, 'min', factor=0.1, patience=5, threshold=0.0001, min_lr=0, eps=1e-08),
-                      'monitor': 'val_loss'}
+      lr_scheduler = {'scheduler': torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer, 'max', factor=0.1, patience=5, threshold=0.0001, min_lr=0, eps=1e-08),
+                      'monitor': 'val_acc'}
       
       
       return [optimizer], [lr_scheduler]
@@ -335,39 +328,40 @@ class DGFRCNN(LightningModule):
       
     def training_step(self, batch, batch_idx):
       
-      imgs = list(image.cuda() for image in batch[0]) 
+      imgs = list(image.to(device = self.device) for image in batch[0]) 
       #imgs = list(image for image in batch[0]) 
 
       targets = []
       for boxes, domain in zip(batch[1], batch[2]):
         target= {}
-        target["boxes"] = boxes.float().cuda()
-        target["labels"] = torch.ones(len(target["boxes"])).long().cuda()
+        target["boxes"] = boxes.float().to(device = self.device)
+        target["labels"] = torch.ones(len(target["boxes"])).long().to(device = self.device)
         targets.append(target)
 
       # fasterrcnn takes both images and targets for training, returns
       #Detection using source images
       
       if(self.mode == 0):
-      
+        
+        loss_dict = {}
         detections = self.detector(imgs, targets)
         loss = sum([loss for detection in detections for loss in detection['losses'].values()])
         
         loss_dict['detection_loss'] = loss
         
+        
         if(self.exp == 'dg'):
           ImgDA_scores = self.ImageDA(self.base_feat['0'])
-          loss_dict['DA_img_loss'] = self.reg_weights[0]*F.cross_entropy(ImgDA_scores, batch[2].to(device=0))
+          loss_dict['DA_img_loss'] = self.reg_weights[0]*F.cross_entropy(ImgDA_scores, batch[2].to(device=self.device))
           IDA_out = self.InsDA(self.box_features)
-          rep_factor = int(IDA_out.shape[0]/self.batch_size)
-          ins_labels = batch[3].reshape(self.batch_size,1).repeat(1, rep_factor).reshape(IDA_out.shape[0])       
-          loss_dict['DA_ins_loss'] = self.reg_weights[1]*F.cross_entropy(IDA_out, ins_labels.to(device=0))
-        
+          rep_factor = int(IDA_out.shape[0]/self.batchsize)
+          ins_labels = batch[2].reshape(self.batchsize,1).repeat(1, rep_factor).reshape(IDA_out.shape[0])      
+          loss_dict['DA_ins_loss'] = self.reg_weights[1]*F.cross_entropy(IDA_out, ins_labels.to(device=self.device))
           ExpImgDA_scores =ImgDA_scores.repeat(1, rep_factor).reshape(IDA_out.shape[0], self.num_domains)
-        
           loss_dict['Cst_loss'] = self.reg_weights[2]*F.mse_loss(IDA_out, ExpImgDA_scores)       
           self.mode = 1
-          
+        
+        #self.mode = 1  
         loss = sum(loss1 for loss1 in loss_dict.values())       
 	                 
       elif(self.mode == 1): #Without recording the gradients for detector, we need to update the weights for classifier weights
@@ -443,8 +437,8 @@ class DGFRCNN(LightningModule):
       targets = []
       for boxes, domain in zip(batch[1], batch[2]):
         target= {}
-        target["boxes"] = boxes.float().cuda()
-        target["labels"] = torch.ones(len(target["boxes"])).long().cuda()
+        target["boxes"] = boxes.float().to(device = self.device)
+        target["labels"] = torch.ones(len(target["boxes"])).long().to(device = self.device)
         targets.append(target)
       
       try:
@@ -496,12 +490,12 @@ if __name__ == '__main__':
   test_dataset = WheatDataset('data/Annots/competition_test.csv', root_dir='data/gwhd_2021/images/', image_set = 'tes', transform=valid_transform)
 
 
-  train_dataloader = torch.utils.data.DataLoader(tr_dataset, batch_size=4, shuffle=True, collate_fn=collate_fn, num_workers=16, drop_last=True)	
+  train_dataloader = torch.utils.data.DataLoader(tr_dataset, batch_size=8, shuffle=True, collate_fn=collate_fn, num_workers=16, drop_last=True)	
   val_dataloader = torch.utils.data.DataLoader(vl_dataset, batch_size=1, shuffle=False,  collate_fn=collate_fn)
   test_dataloader = torch.utils.data.DataLoader(test_dataset, batch_size=1, shuffle=False,  collate_fn=collate_fn)
   
   # Instantiating the detector
-  detector = DGFRCNN(2, 4, args.exp, args.reg_weights) # Num classes + 1 and batch_size
+  detector = DGFRCNN(2, 8, args.exp, args.reg_weights) # Num classes + 1 and batch_size
 
   if os.path.exists(NET_FOLDER+'/'+weights_file+'.ckpt'): 
     detector.load_state_dict(torch.load(NET_FOLDER+'/'+weights_file+'.ckpt')['state_dict'])
