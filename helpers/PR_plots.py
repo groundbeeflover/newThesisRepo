a869a05fc33b522f362270061cc11a78684cdb75
karthikiitm87 <karthikiitm87@gmail.com>
2024-04-14 21:35:51 +0100

    Updated the generation of PR Plots

--- helpers/PR_plots.py
+++ helpers/PR_plots.py
@@ -14,15 +14,21 @@ ious = [0.1, 0.5, 0.75]
 colors = ['red', 'green', 'blue']
 Rthres = [i / 100 for i in range(101)]
 for index, PR in enumerate(frcnn_precision[:, :, 0, 0, -1]):
-  plt.plot(Rthres, PR.numpy(), linestyle='dotted', color = colors[index], linewidth=4, label='FRCNN(IoU = '+str(ious[index])+')')
+  plt.plot(Rthres, PR.numpy(), linestyle='dotted', color = colors[index], linewidth=4)
 for index, PR in enumerate(dgfrcnn_precision[:, :, 0, 0, -1]):
-  plt.plot(Rthres, PR.numpy(), color = colors[index], linewidth=4, label='DGFRCNN(IoU = '+str(ious[index])+')')
+  plt.plot(Rthres, PR.numpy(), color = colors[index], linewidth=4)
       
-plt.xlabel('Recall', fontsize=22)
-plt.xticks(fontsize=20)
-plt.ylabel('Precision', fontsize=22)
-plt.yticks(fontsize=20)
-plt.title('PR Curves (DGFRCNN vs FRCNN)', fontsize=22)
-plt.legend(loc='lower left', fontsize=10.5)
+plt.xlabel('Recall', fontsize=30)
+plt.xticks(fontsize=30)
+plt.ylabel('Precision', fontsize=30)
+plt.yticks(fontsize=30)
+plt.title('DGFRCNN vs FRCNN', fontsize=30)
+plt.tight_layout()
+
+#dotted_patch = plt.Line2D([], [], color='black', linestyle='dotted', label='FRCNN', linewidth=4)
+#solid_patch = plt.Line2D([], [], color='black', label='DGFRCNN', linewidth=4)
+#plt.legend(loc='upper right', handles=[dotted_patch, solid_patch], fontsize=20)
+
+#plt.legend(loc='best', fontsize=26)
 plt.savefig('PRCurves_FRCNN.png')  
 
